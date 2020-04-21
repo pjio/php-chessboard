17ab384a8be4652b82275f70e7269c53e67a2086
Carl-Philipp Jung <mail+git@philipp-jung.net>
2020-04-21 16:35:28 +0200

    Adds BishopRule

--- src/Rule/BishopRule.php
+++ src/Rule/BishopRule.php
@@ -1,14 +1,65 @@
 <?php
 namespace Pjio\Chessboard\Rule;
 
-use Pjio\Chessboard\MoveValidatorInterface;
 use Pjio\Chessboard\Board\Chessboard;
+use Pjio\Chessboard\Board\Square;
 use Pjio\Chessboard\Move;
+use Pjio\Chessboard\MoveValidatorInterface;
+use Pjio\Chessboard\Pieces\Bishop;
 
-class BishopRule implements MoveValidatorInterface
+class BishopRule extends BaseRule implements MoveValidatorInterface
 {
+    protected const PIECE_TYPE = Bishop::class;
+
     public function isValidMove(Move $move, Chessboard $chessboard): bool
     {
-        return true;
+        if ($this->isDifferentPieceType($move, $chessboard)
+            || $this->isDifferentPlayer($move, $chessboard)
+            || $this->isBlockedByOwnPiece($move, $chessboard)
+        ) {
+            return false;
+        }
+
+        $from = $move->getFrom();
+        $to   = $move->getTo();
+
+        $fileDist = abs($from->getFile() - $to->getFile());
+        $rankDist = abs($from->getRank() - $to->getRank());
+
+        // $from and $to must be diagonal to each other
+        if ($fileDist !== $rankDist) {
+            return false;
+        }
+
+        if (!$this->isFreePath($move->getFrom(), $move->getTo(), $chessboard)) {
+            return false;
+        }
+
+        return !$this->isOwnKingCheckedAfterMove($move, $chessboard);
+    }
+
+    private function isFreePath(Square $from, Square $to, Chessboard $chessboard): bool
+    {
+        $moveFile = $from->getFile() < $to->getFile() ? 1 : -1;
+        $moveRank = $from->getRank() < $to->getRank() ? 1 : -1;
+        $startFile = $from->getFile() + $moveFile;
+        $startRank = $from->getRank() + $moveRank;
+        $exceedFile = $to->getFile() + $moveFile;
+        $exceedRank = $to->getRank() + $moveRank;
+
+        for ($file = $startFile; $file != $exceedFile; $file += $moveFile) {
+            for ($rank = $startRank; $rank != $exceedRank; $rank += $moveRank) {
+                $current = new Square($file, $rank);
+                if ($current == $to) {
+                    return true;
+                }
+
+                if (null !== $chessboard->getPieceBySquare($current)) {
+                    return false;
+                }
+            }
+        }
+
+        return false;
     }
 }
---
+++ tests/Rule/BishopRuleTest.php
@@ -0,0 +1,117 @@
+<?php
+namespace Tests;
+
+require_once __DIR__ . '/MoveHelper.php';
+
+use PHPUnit\Framework\TestCase;
+use Pjio\Chessboard\Board\Chessboard;
+use Pjio\Chessboard\Board\ChessboardSerializer;
+use Pjio\Chessboard\Board\Square;
+use Pjio\Chessboard\Move;
+use Pjio\Chessboard\Rule\BishopRule;
+use Tests\MoveHelper;
+
+class BishopRuleTest extends TestCase
+{
+    private BishopRule $bishopRule;
+    private ChessboardSerializer $chessboardSerializer;
+
+    public function setUp(): void
+    {
+        $this->bishopRule = new BishopRule();
+        $this->chessboardSerializer = new ChessboardSerializer();
+    }
+
+    /**
+     * @dataProvider provideMoves
+     */
+    public function testIsValidMove(string $board, Move $move, bool $expected)
+    {
+        /** @var Chessboard $chessboard */
+        $chessboard = $this->chessboardSerializer->unserialize($board);
+
+        $actual = $this->bishopRule->isValidMove($move, $chessboard);
+
+        $this->assertSame($expected, $actual);
+    }
+
+    public function provideMoves(): array
+    {
+        $moveList = [];
+        $moveHelper = new MoveHelper();
+
+        $testScenario = 'empty_board';
+        $fromSquare = new Square(Square::FILE_D, Square::RANK_5);
+        $board = <<< EOF
+                       
+                       
+                       
+         bb            
+                       
+                       
+                       
+                       
+EOF;
+        $validMoves = <<< EOF
+bb                bb   
+   bb          bb      
+      bb    bb         
+                       
+      bb    bb         
+   bb          bb      
+bb                bb   
+                     bb
+EOF;
+        $moveList = array_merge($moveList, $moveHelper->getMoves($testScenario, $fromSquare, $board, $validMoves));
+
+        $testScenario = 'blocked';
+        $fromSquare = new Square(Square::FILE_D, Square::RANK_5);
+        $board = <<< EOF
+                       
+                       
+      bp    bp         
+         bb            
+      bp    bp         
+                       
+                       
+                       
+EOF;
+        $validMoves = <<< EOF
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+EOF;
+        $moveList = array_merge($moveList, $moveHelper->getMoves($testScenario, $fromSquare, $board, $validMoves));
+
+        $testScenario = 'capture';
+        $fromSquare = new Square(Square::FILE_D, Square::RANK_5);
+        $board = <<< EOF
+                       
+   wp          wp      
+                       
+         bb            
+                       
+   wp          wp      
+                       
+                       
+EOF;
+        $validMoves = <<< EOF
+                       
+   bb          bb      
+      bb    bb         
+                       
+      bb    bb         
+   bb          bb      
+                       
+                       
+EOF;
+        $moveList = array_merge($moveList, $moveHelper->getMoves($testScenario, $fromSquare, $board, $validMoves));
+
+        return $moveList;
+    }
+}
